6a08199e85e70247f73d1c0120e70e4b63b80e23
Kevin Choi <kevinyoungchoi@gmail.com>
2019-04-03 20:01:13 -0400

    resume changes

---
+++ 2ndPortfolio/precache-manifest.1ff66180eb3c65b5f705722a34fe65d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "445c1791466de275edf3",
+    "url": "/static/css/main.ea4671cb.chunk.css"
+  },
+  {
+    "revision": "445c1791466de275edf3",
+    "url": "/static/js/main.b5c10eb9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "3d7e19fd1e172f9c7de200c2c3b3fcd0",
+    "url": "/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+  "imagebaseurl": "https://github.com/choik305/2ndPortfolio.git",
+  "name": "Kevin Choi",
+  "role": "Full Stack Developer Developer"
+}
+export default resumeData
